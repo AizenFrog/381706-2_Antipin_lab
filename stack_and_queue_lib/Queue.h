9544cd6dab405a25b46991b30cf0b37e4757578e
381706-2AntipinAleksandr <antipin.aleksandr.1999@mail.ru>
2019-02-06 18:47:46 +0300

    queue with this

--- stack_and_queue_lib/Queue.h
+++ stack_and_queue_lib/Queue.h
@@ -13,9 +13,9 @@ public:
   Queue(const Queue<T>& A);
   void Put(const T& A);
   T Get();
-  T GetWithoutDelete() const;
-  bool IsFull() const;
-  bool IsEmpty() const;
+  T GetWithoutDelete();
+  bool IsFull();
+  bool IsEmpty();
 };
 
 template<class T>
@@ -29,22 +29,22 @@ template<class T>
 Queue<T>::Queue(const int _Len)
 {
   if (_Len < 0)
-    except->except_throw(106);
+    this->except.except_throw(106);
   else if (_Len == 0)
   {
-    Len = 0;
-    Mem = NULL;
-    Index = 0;
+    this->Len = 0;
+    this->Mem = NULL;
+    this->Index = 0;
     counts = 0;
     IndexFirst = 0;
   }
   else
   {
-    Len = _Len;
-    Mem = new T[Len];
+    this->Len = _Len;
+    this->Mem = new T[Len];
     for (int i = 0; i < Len; i++)
       Mem[i] = 0;
-    Index = 0;
+    this->Index = 0;
     IndexFirst = 0;
     counts = 0;
   }
@@ -53,11 +53,11 @@ Queue<T>::Queue(const int _Len)
 template<class T>
 Queue<T>::Queue(const Queue<T>& A)
 {
-  Len = A->Len;
-  Mem = new T[Len];
+  this->Len = A->Len;
+  this->Mem = new T[Len];
   for (int i = 0; i < Len; i++)
-    Mem[i] = A->Mem[i];
-  Index = A->Index;
+    this->Mem[i] = A->Mem[i];
+  this->Index = A->Index;
   IndexFirst = A.IndexFirst;
   counts = A.counts;
 }
@@ -67,17 +67,17 @@ void Queue<T>::Put(const T& A)
 {
   if (IsFull() == false)
   {
-    Mem[Index] = A;
+    this->Mem[Index] = A;
     counts++;
-    if (counts != Len && Index != Len)
-      Index++;
-    else if (counts != Len && Index == Len)
-      Index = 0;
-    else if (counts == Len)
-      Index = 0;
+    if (counts != this->Len && Index != this->Len)
+      this->Index++;
+    else if (counts != this->Len && Index == this->Len)
+      this->Index = 0;
+    else if (counts == this->Len)
+      this->Index = 0;
   }
   else
-    except->except_throw(107);
+    this->except.except_throw(107);
 }
 
 template<class T>
@@ -86,49 +86,49 @@ T Queue<T>::Get()
   if (IsEmpty() == false)
   {
     counts--;
-    T A = Mem[IndexFirst];
-    if (counts != 0 && IndexFirst != Len - 1)
+    T A = this->Mem[IndexFirst];
+    if (counts != 0 && IndexFirst != this->Len - 1)
       IndexFirst++;
-    else if (counts != 0 && IndexFirst == Len - 1)
+    else if (counts != 0 && IndexFirst == this->Len - 1)
       IndexFirst = 0;
     else if (counts == 0)
-      IndexFirst = Index = 0;
+      IndexFirst = this->Index = 0;
     return A;
   }
   else
-    except->except_throw(108);
+    this->except.except_throw(108);
 }
 
 template<class T>
-T Queue<T>::GetWithoutDelete() const
+T Queue<T>::GetWithoutDelete()
 {
   if (IsEmpty() == false)
   {
-    T A = Mem[IndexFirst];
+    T A = this->Mem[IndexFirst];
     return A;
   }
   else
-    except->except_throw(108);
+    this->except.except_throw(108);
 }
 
 template<class T>
-bool Queue<T>::IsFull() const
+bool Queue<T>::IsFull()
 {
-  if (counts == Len)
+  if (counts == this->Len)
     return true;
-  else if (counts > Len)
-    except->except_throw(109);
+  else if (counts > this->Len)
+    this->except.except_throw(109);
   else
     return false;
 }
 
 template<class T>
-bool Queue<T>::IsEmpty() const
+bool Queue<T>::IsEmpty()
 {
   if (counts == 0)
     return true;
-  else if (counts > Len)
-    except->except_throw(110);
+  else if (counts > this->Len)
+    this->except.except_throw(110);
   if (counts > 0)
     return false;
 }
